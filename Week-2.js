2cf9591fc54e2f9838156617cf3345c2d3827df0
Brian Harrington <brianharrington2014@gmail.com>
2018-06-18 09:25:15 -0700

    Complete wk 2

--- Week-2.js
+++ Week-2.js
@@ -1,6 +1,22 @@
 // 1. Write an anonymous function that takes one argument of type number and decides if that number is evenly divisble by three or not. If it is, print the number and "is divisible by three". If it is not, print that the number "is not divisble by three".
 
+let divisibleByThree = function(num) {
+	if (num % 3 === 0) {
+		console.log(num + " is divisible by three!");
+	} else {
+		console.log(num + " is not divisible by three.");
+	}
+}
+
+divisibleByThree(15)
+
 // 2. Write about yourself using an object. Include at least three properties of you and store your object in a variable with your name.
+
+var brianHarrington = {
+						favoriteColor: "Blue",
+						height: 6,
+						hairColor: "Black"
+					}
 
 // 3. Given the object below, fill in how to find specific information:
 
@@ -15,34 +31,52 @@ var bicycle = {
 }
 
 // Log the type of bicycle:
-console.log()
+console.log(bicycle.type)
 
 // Log just the bell from the list of gear
-console.log()
+console.log(bicycle.gear[2])
 
 // Log the correct PSI for the tires
-console.log()
+console.log(bicycle.wheels.specs[2])
 
-// 4. Have the function AlphabetSoup(str) take the "str" parameter being passed and return a string with the letters in alphabetical order (ie. hello becomes ehllo). Assume no numbers or punctuation symbols will not be included in the parameter. 
+// 4. Have the function AlphabetSoup(str) take the "str" parameter being passed and return a string with the letters in alphabetical order (ie. hello becomes ehllo). Assume no numbers or punctuation symbols will not be included in the parameter.
 
 // Input:"hooplah"
 // Output:"ahhloop"
 
-function AlphabetSoup(str) {
+function alphabetSoup(str) {
 
-    // your code goes here  
-	
-	return str; 
+		// Method 1
+    let newStr = str.toLowerCase().split("").sort().join(",")
+		str = newStr
+
+		// Method 2 - String > Array > Joined Array
+		// let arrStr = str.toLowerCase().split("")
+		// arrStr.sort()
+		// let finalStr = arrStr.join(",")
+		// str = finalStr
+
+	return str;
 }
 
-// keep this function call here 
+alphabetSoup("Brian")
 
-AlphabetSoup(hooplah);   
+// keep this function call here
+
+alphabetSoup("hooplah"); // Brian: This call string wasn't wrapped in " "
 
 // 5. Given the arrays below, use a for loop to print one value from each array concatenated together. How would your code need to change to accomodate arrays of different lengths?
 
 var nums = [1, 5, 88, 2, 5, 42, 57, 101]
 
 var nouns = ["ducks", "telephone booth", "the enterprise", "robots", "amazon", "eraser", "zafod", "a"]
+
+function concat() {
+	for (var i = 0; i < nums.length; i++) {
+		console.log(nums[i] + " " + nouns[i])
+	}
+}
+
+concat()
 
 // output of the first function should be: "1 ducks"
--- Week-2.md
+++ Week-2.md
@@ -2,15 +2,28 @@
 
 These are potential interview questions. Try your best to answer each question on your own before looking up the answer online.
 
-#### 1. Name all of the data types in Javascript, mark which are primitives. 
+#### 1. Name all of the data types in Javascript, mark which are primitives.
 
   //Your Answer
-  
-  
+  primitives
+  - string
+  - number
+  - boolean
+  - undefined
+  - NaN
+  - null
+  - object
+
   //Googled Answer
+  - string
+  - number
+  - boolean
+  - undefined
+  - null
+  - object
+  - SYMBOL
 
-
-#### 2. Look at this Javascript and try to predict, what will the browser's console show? 
+#### 2. Look at this Javascript and try to predict, what will the browser's console show?
 
 ``` javascript
 
@@ -24,31 +37,43 @@ logIt();
 
 ```
 
-first Guess: 
+first Guess:
+== 'outside'
 
 
-Then, past the code in your console and explain why you were right/wrong. 
+Then, past the code in your console and explain why you were right/wrong.
+
+ == The console read undefined. I think it showed undefined because, while the text variable is global, it was not being passed into the function and thus the console.log, which is itself a method, couldn't utilize it.
 
 
 #### 3. What is JSON? How does it relate to javascript objects?
 
   //Your Answer
-  
-  
+  JavaScript Object Notation. It is used to transport data as objects - to APIs, across components, etc
+
   //Googled Answer
+  JavaScript Object Notation readable format for storing and exchanging data. It is written in object notation and any JavaScript object can be converted into JSON and send that JSON to the server and vice versa.
 
 
 #### 4. Describe a closure, what is it good for and how do you recognize one?
 
   //Your Answer
-  
-  
+  Closures are functions with their own privately scope variables. Closures are the returned function within functions.
+
+
   //Googled Answer
-  
+  A closure is a function having access to the parent scope, even after the parent function has closed.
+
 
 #### 5. What's the difference between =, ==, and === in JavaScript?
 
   //Your Answer
-  
-  
+  = assignment
+  == loose equals (truthy / falsey). Doesn't compare exact type.
+  === Compares type and ensures both values on either side are exactly the same
+
+
   //Googled Answer
+  = assignment
+  == equality operator: Type conversion is done and can equate values to be equal
+  === identity operator: Does not type convert, so the values must be the same to return true
